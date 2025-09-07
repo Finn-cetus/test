cf9e98a146ba31c165b0602e64ca086b8fe22ccc
Finn-cetus <chunggtest2006@gmail.com>
2025-09-07 23:48:20 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "render-ddos-script",
+  "version": "1.0.0",
+  "description": "",
+  "main": "test.js",
+  "scripts": {
+    "start": "node test.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "gradient-string": "^2.0.2",
+    "http-proxy-agent": "^7.0.2",
+    "https-proxy-agent": "^7.0.4",
+    "socks-proxy-agent": "^8.0.3"
+  }
+}
